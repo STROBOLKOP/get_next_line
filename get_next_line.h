b4cf654bd920717580c45e4bf72007157d4b6cb8
Elias Fret <efret@a1-r2-p7.s19.be>
2023-11-27 16:24:43 +0100

    Removed libft, we aren't allowed to use it yet.

--- get_next_line.h
+++ get_next_line.h
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 11:03:29 by efret             #+#    #+#             */
-/*   Updated: 2023/11/21 11:29:55 by efret            ###   ########.fr       */
+/*   Created: 2023/11/27 16:19:24 by efret             #+#    #+#             */
+/*   Updated: 2023/11/27 16:20:35 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-
-	if (size == 0)
-		return (ft_strlen(src));
-	i = 0;
-	while (i < size - 1 && src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (ft_strlen(src));
-}
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/20 15:04:04 by efret             #+#    #+#             */
-/*   Updated: 2023/11/21 15:37:14 by efret            ###   ########.fr       */
+/*   Created: 2023/11/27 16:19:02 by efret             #+#    #+#             */
+/*   Updated: 2023/11/27 16:19:03 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
-{
-	int	i;
-
-	i = ft_strlen(s);
-	if ((char)c == '\0')
-		return ((char *)&s[i]);
-	while (--i >= 0)
-		if (s[i] == (unsigned char)c)
-			return ((char *)&s[i]);
-	return (NULL);
-}
