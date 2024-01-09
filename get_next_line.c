bad5f777b03855ba081e54cf6e682c8dcb336144
Elias Fret <efret@student.s19.be>
2024-01-09 17:32:38 +0100

    First actual working program.
    A modified ft_strjoin was used to join the line with new data from the buffer. Lot's still to test.

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,101 @@
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:18:29 by efret             #+#    #+#             */
-/*   Updated: 2023/11/27 19:31:13 by efret            ###   ########.fr       */
+/*   Updated: 2024/01/09 17:30:35 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dptr;
+	unsigned char	*sptr;
+
+	if (!dest && !src && n)
+		return (NULL);
+	i = 0;
+	dptr = (unsigned char *)dest;
+	sptr = (unsigned char *)src;
+	while (i < n)
+	{
+		dptr[i] = sptr[i];
+		i++;
+	}
+	return (dest);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(t_file *file, size_t start)
+{
+	char			*res;
+	size_t			buf_data_len;
+
+	buf_data_len = file->pos - start;
+	res = malloc(sizeof(char) * (file->len + buf_data_len + 1));
+	if (!res)
+		return (NULL);
+	for (size_t i = 0; i < file->len + buf_data_len; i++)
+		res[i] = '0';
+	ft_memcpy(res, file->line, file->len);
+	ft_memcpy(&res[file->len], &file->buffer[start], buf_data_len);
+	res[file->len + file->pos - start] = 0;
+	free(file->line);
+	file->len += buf_data_len;
+	return (res);
+}
+
 char	*get_next_line(int fd)
 {
-	static t_file	file;
-	size_t			i;
+	static t_file	*file;
+	size_t			start;
 
-	if (fd < 0 || (file.fd && file.fd != fd))
+	/*
+	 * If pos < buffersize; then continue from buffer.
+	 * no buffer left to loop over to check but still no newline,
+	 * then read from fd again and
+	 * loop over the new buffer data in search for the next newline.
+	 * always allocate the full length again and free the prev length.
+	 */
+	file = malloc(sizeof(t_file));
+	if (!file)
 		return (NULL);
-	if (!file.fd)
-		file.fd = fd;
-	if (!read(file.fd, file.buffer, BUFFER_SIZE))
+	if (fd < 0 || (file->fd && file->fd != fd))
+		return (NULL);
+	file->fd = fd;
+	if (file->line)
+		free(file->line);
+	file->len = 0;
+	while (1)
 	{
-		if (file.line)
-			free(file.line);
-		return (NULL);
+		if (!(file->pos) || file->pos == BUFFER_SIZE)
+		{
+			file->pos = 0;
+			if (read(file->fd, file->buffer, BUFFER_SIZE) <= 0)
+				break;
+		}
+		start = file->pos;
+		while (file->buffer[file->pos])
+		{
+			file->pos++;
+			if (file->buffer[file->pos-1] == '\n')
+				break;
+		}
+		file->line = ft_strjoin(file, start);
+		if (file->line[file->len - 1] == '\n')
+			break;
 	}
-	i = 0;
-	while (file.buffer[i] && file.buffer[i] != '\n')
-		i++;
-	if (file.line)
-		free(file.line);
-	file.line = malloc(sizeof(char) * (i + 1));
-	file.line[i] = 0;
-	while (i-- > 0)
-		file.line[i] = file.buffer[i];
-	
-	return (file.line);
+	return (file->line);
 }
 
 #include <stdio.h>
@@ -50,15 +113,9 @@ int	main(void)
 		printf("Could not open file.\n");
 		return (0);
 	}
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 5; i++)
 		printf("%s", get_next_line(fd));
 
-	char	buf[BUFFER_SIZE];
-	for (int i = 0; i < 10; i++)
-	{
-		read(fd, buf, BUFFER_SIZE);
-		printf("%s", buf);
-	}
 	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:19:24 by efret             #+#    #+#             */
-/*   Updated: 2023/11/27 17:38:51 by efret            ###   ########.fr       */
+/*   Updated: 2024/01/09 16:48:24 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
-
-# define BUFFER_SIZE 1
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
 
 typedef struct s_file
 {
@@ -23,6 +24,7 @@ typedef struct s_file
 	size_t	pos;
 	char	buffer[BUFFER_SIZE];
 	char	*line;
+	size_t	len;
 }			t_file;
 
 char	*get_next_line(int fd);
