c5db0316491801480803729789c5a2afc43f8734
Elias Fret <efret@student.s19.be>
2024-01-22 16:54:57 +0100

    Added bonus, made me realize I forgot to add +1 to buffersize.

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,40 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:18:29 by efret             #+#    #+#             */
-/*   Updated: 2024/01/22 15:48:05 by efret            ###   ########.fr       */
+/*   Updated: 2024/01/22 16:51:35 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1] = "";
+	static char	buffers[1024][BUFFER_SIZE + 1];
 	char		*line;
 	int			read_return;
 
 	line = NULL;
-	if (buf[0])
-		if (ft_append_line(&line, buf))
+	if (buffers[fd][0])
+		if (ft_append_line(&line, buffers[fd]))
 			return (line);
 	while (1)
 	{
-		read_return = read(fd, buf, BUFFER_SIZE);
+		read_return = read(fd, buffers[fd], BUFFER_SIZE);
 		if (read_return < 0)
 		{
 			if (line)
 				free(line);
-			ft_bzero(buf, BUFFER_SIZE);
+			ft_bzero(buffers[fd], BUFFER_SIZE);
 			return (NULL);
 		}
 		if (read_return == 0)
 			return (line);
-		if (ft_append_line(&line, buf))
+		if (ft_append_line(&line, buffers[fd]))
 			return (line);
 	}
 	return (NULL);
@@ -45,21 +45,36 @@ char	*get_next_line(int fd)
 #include <fcntl.h>
 int	main(void)
 {
-char	*line;
-int	fd = open("test.txt", O_RDONLY);
-if (fd < 0)
-{
-printf("Could not open file.\n");
-return (0);
-}
-for (int i = 0; i < 3; i++)
-{
-line = get_next_line(fd);
-printf("%s", line);
-if (line)
-free(line);
-}
-close(fd);
-return (0);
+	char	*line;
+	int	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Could not open file.\n");
+		return (0);
+	}
+	int	fd2 = open("test2.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Could not open file.\n");
+		return (0);
+	}
+	int	fd3 = open("test3.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Could not open file.\n");
+		return (0);
+	}
+	int	files[3] = {fd, fd2, fd3};
+	for (int i = 0; i < 6; i++)
+	{
+		line = get_next_line(files[i % 3]);
+		printf("%s", line);
+		if (line)
+			free(line);
+	}
+	close(fd);
+	close(fd2);
+	close(fd3);
+	return (0);
 }
 */
---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 16:19:24 by efret             #+#    #+#             */
+/*   Updated: 2024/01/22 14:37:27 by efret            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlen_newline(const char *s);
+void	ft_bzero(void *s, size_t n);
+int		ft_append_line(char **line, char *buf);
+#endif
